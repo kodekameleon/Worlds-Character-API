cb908450a5ac18df6aa4707e570230bf1c6391a4
Justin Joyce <jjoyce@aptos.com>
2020-05-26 17:10:44 +1000

    Update classes and races

--- src/services/classes.js
+++ src/services/classes.js
@@ -3,6 +3,58 @@ import {service} from "../kameleon-web";
 
 service["GET /{apiVersion}/classes/{classId}"] = getClass;
 async function getClass(event) {
-  return event;
+  console.log(event);
+  return {
+    items: [
+      {
+        uniqueId: "srd:barbarian",
+        name: "Barbarian"
+      },
+      {
+        uniqueId: "srd:bard",
+        name: "Bard"
+      },
+      {
+        uniqueId: "srd:cleric",
+        name: "Cleric"
+      },
+      {
+        uniqueId: "srd:druid",
+        name: "Druid"
+      },
+      {
+        uniqueId: "srd:fighter",
+        name: "Fighter"
+      },
+      {
+        uniqueId: "srd:monk",
+        name: "Monk"
+      },
+      {
+        uniqueId: "srd:paladin",
+        name: "Paladin"
+      },
+      {
+        uniqueId: "srd:ranger",
+        name: "Ranger"
+      },
+      {
+        uniqueId: "srd:rogue",
+        name: "Rogue"
+      },
+      {
+        uniqueId: "srd:sorcerer",
+        name: "Sorcerer"
+      },
+      {
+        uniqueId: "srd:warlock",
+        name: "Warlock"
+      },
+      {
+        uniqueId: "srd:wizard",
+        name: "Wizard"
+      }
+    ]
+  };
 }
 
